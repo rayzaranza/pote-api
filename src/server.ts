f2260d6e41b7d6da0bd2713a4dc7c7acbcaf88c2
rayzaranza <rayzaranza@gmail.com>
2026-04-27 17:05:34 -0300

    feat: shudown server on terminal close signal

--- src/server.ts
+++ src/server.ts
@@ -7,15 +7,19 @@ dotenv.config();
 app.listen(
   { port: Number(process.env.PORT) || 3000, host: "0.0.0.0" },
   (error) => {
-  if (error) {
-    app.log.error(error);
-    process.exit(1);
-  }
+    if (error) {
+      app.log.error(error);
+      process.exit(1);
+    }
   },
 );
 
-process.on("SIGTERM", async () => {
+async function shutdown() {
+  app.log.info("Encerrando servidor...");
   await pool.end();
   await app.close();
   process.exit(0);
-});
+}
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
