8fa274f1ceb77a9f169cb4b79229978f625371d6
rayzaranza <rayzaranza@gmail.com>
2026-05-03 17:32:38 -0300

    feat: check for zod errors on global error handler

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import {
   validatorCompiler,
 } from "@fastify/type-provider-zod";
 import multipart from "@fastify/multipart";
+import { ZodError } from "zod";
 
 export const app = Fastify({ logger: true });
 
@@ -21,10 +22,18 @@ app.setErrorHandler((error: FastifyError, _request, reply) => {
       .send({ error: error.message, code: error.statusCode });
   }
 
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      error: "Erro de validação",
+      ...(process.env.NODE_ENV !== "production" && {
+        details: error.issues,
+      }),
+    });
+  }
+
   if (error.validation) {
     return reply.status(400).send({
       error: "Erro de validação",
-      message: error.message,
       ...(process.env.NODE_ENV !== "production" && {
         details: error.validation,
       }),
