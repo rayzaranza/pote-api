8b15976a32e08b742337f544d8337c8526db63c6
rayzaranza <rayzaranza@gmail.com>
2026-05-03 17:31:10 -0300

    types: add assets types and schemas

--- src/types/assets.ts
+++ src/types/assets.ts
@@ -1,6 +1,43 @@
-export enum AssetType {
-  video = "video",
-  image = "image",
-  "3d" = "3d",
-  document = "document",
+import { type MultipartFile } from "@fastify/multipart";
+import { z } from "zod";
+
+export const AssetTypeSchema = z.enum(["image", "video", "3d", "document"]);
+export type AssetType = z.infer<typeof AssetTypeSchema>;
+
+export const AssetInsertSchema = z.object({
+  name: z.string().min(2),
+  description: z.string().nullish(),
+  type: AssetTypeSchema,
+  collection_id: z.uuid().nullish(),
+});
+
+export interface FileData {
+  url: string;
+  size: number;
+  mime_type: string;
+  width: number | null;
+  height: number | null;
+}
+
+export type AssetInsert = z.infer<typeof AssetInsertSchema> & FileData;
+
+export interface Asset extends FileData {
+  id: string;
+  name: string;
+  description: string | null;
+  type: AssetType;
+  created_at: string;
+  updated_at: string;
+  collection_id: string | null;
+  user_id: string;
+}
+
+export type PublicAsset = Omit<Asset, "user_id">;
+
+export interface AssetUploadBody {
+  file: MultipartFile;
+  name: string;
+  type: string;
+  description?: string;
+  collection_id?: string;
 }
