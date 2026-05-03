466ad628d754cfa661b0ca3cef1b141d6ddf8e53
rayzaranza <rayzaranza@gmail.com>
2026-05-02 22:09:41 -0300

    refactor: set error classes name with super

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -4,21 +4,20 @@ export class AppError extends Error {
     public statusCode: number,
   ) {
     super(message);
-    this.name = "AppError";
     this.statusCode = statusCode;
   }
 }
 
 export class ConflictError extends AppError {
   constructor(message = "Already exists") {
-    super(message, 409);
-    this.name = "ConflictError";
+    super(message, 409, "CONFLICT_ERROR");
   }
 }
 
 export class ValidationError extends AppError {
   constructor(message: string) {
-    super(message, 400);
-    this.name = "ValidationError";
+    super(message, 400, "VALIDATION_ERROR");
+  }
+}
   }
 }
