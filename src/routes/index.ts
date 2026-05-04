d419c14150b19bcbfe338b854712cfc8e299a35a
rayzaranza <rayzaranza@gmail.com>
2026-05-04 02:08:12 -0300

    refactor: split auth from users routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,18 @@
 import { type FastifyInstance } from "fastify";
 import { authPlugin } from "../plugins/auth.js";
 
+import { authRoutes } from "./auth.js";
 import { usersRoutes } from "./users.js";
 import { collectionsRoutes } from "./collections.js";
 import { assetsRoutes } from "./assets.js";
 
 export async function routes(app: FastifyInstance) {
-  await app.register(usersRoutes, { prefix: "/users" });
+  await app.register(authRoutes, { prefix: "/auth" });
 
   await app.register(async (protectedApp) => {
     await protectedApp.register(authPlugin);
     await protectedApp.register(collectionsRoutes, { prefix: "/collections" });
     await protectedApp.register(assetsRoutes, { prefix: "/assets" });
+    await protectedApp.register(usersRoutes, { prefix: "/users" });
   });
 }
